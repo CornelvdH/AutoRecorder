66d88ecfc83b1a12ea6e7fdcc752976a12e18dc2
Cornel van der Heiden <cornel@nenicto.com>
2014-04-20 17:07:06 +0200

    Added sample backend PHP parser.

---
+++ AutoRecorder/backend/data.php
@@ -0,0 +1,10 @@
+<?php
+
+if(isset($_GET['version'])){
+    $file = $_GET['version'] . ".txt";
+    if(file_exists($file)){
+        $f = file_get_contents($file);
+        $exp = explode(PHP_EOL, $f);
+        echo json_encode($exp);
+    }
+}
